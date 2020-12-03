ba1eab00ec42eb57f64e963f83585e078dfa1209
Davi <davi@agst.dev>
2020-12-03 00:10:04 -0300

    Hotfix adição de endereço e adição de HTML
    Houve uma correção de nome errôneo de variável no script de adição de endereço e também
    foi adicionado um HTML base para o formulário de adição de endereço

--- novo-endereco/index.php
+++ novo-endereco/index.php
@@ -41,7 +41,7 @@ function post_return() {
     
     try {
         $query = $pdo->prepare($sql);
-        $query->execute([$payload["cep"], $payload["logradouro"], $payload["bairro"], $payload["cidade"], $payload["estado"]]);
+        $query->execute([$_POST["cep"], $_POST["logradouro"], $_POST["bairro"], $_POST["cidade"], $_POST["estado"]]);
     }
     catch (Exception $e) {
         return_err("Houve um erro na adição do endereço", 500);
@@ -53,4 +53,61 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     exit();
 }
 ?>
-<!-- HTML para a página visual será inserido aqui -->
+<!doctype html>
+<html lang="pt-BR">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-CuOF+2SnTUfTwSZjCXf01h7uYhfOBuxIhGKPbfEJ3+FqH/s6cIFN9bGr1HmAg4fQ" crossorigin="anonymous">
+
+    <title>Clínica XYZ: Cadastro de Endereço</title>
+  </head>
+  <body>
+    <div class="container">
+      <form class="row g-2" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+        <div class="col-sm-3">
+          <div class="form-floating">
+            <input type="text" class="form-control" id="cep" placeholder="00000000000" name="cep">
+            <label for="cep">CEP</label>
+          </div>
+        </div>
+        <div class="col-sm-9">
+          <div class="form-floating">
+            <input type="text" class="form-control" id="bairro" placeholder="Bairro" name="bairro" required>
+            <label for="bairro">Bairro</label>
+          </div>
+        </div>
+        <div class="col-sm-12">
+          <div class="form-floating">
+            <input type="text" class="form-control" id="logradouro" placeholder="Rua" name="logradouro" required>
+            <label for="logradouro">Logradouro</label>
+          </div>
+        </div>
+        <div class="col-sm-9">
+          <div class="form-floating">
+            <input type="text" class="form-control" id="cidade" placeholder="cidade" name="cidade" required>
+            <label for="cidade">Cidade</label>
+          </div>
+        </div>
+        <div class="col-sm-3">
+          <div class="form-floating">
+            <select class="form-select" id="estado" aria-labe="estado" name="estado" required>
+              <option selected>MG</option>
+              <option>SP</option>
+            </select>
+            <label for="estado">Estado</label>
+          </div>
+        </div>
+
+        <div class="col-12">
+          <button type="submit" class="btn btn-primary">
+            Enviar
+          </button>
+        </div>
+      </form>
+    </div>
+  </body>
+</html>
