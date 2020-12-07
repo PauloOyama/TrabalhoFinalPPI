08fad82a78c58f54a1d021775589a0355e16f0c6
Paulo Oyama <papaloyama@gmail.com>
2020-12-07 10:22:44 -0300

    Add cadastro paciente html and css

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # Entrypoint para instalar as extensões necessárias do servidor PHP
 
-docker-php-ext-install pdo pdo_mysql && \
-php -S 0.0.0.0:5000
+docker-php-ext-install pdo pdo_mysql && php -S 0.0.0.0:5000
